feda3750c8dda2e6b9e1bd6e47f2641ccdb5a852
Your Name <you@example.com>
2023-02-22 14:21:09 -0500

    Using Joi to validate and sanitize user input

--- models/user.js
+++ models/user.js
@@ -1,55 +1,56 @@
 const mongoose = require('mongoose');
-// const Joi = require('joi')
+const Joi = require('joi');
+const phoneRegex = /^[\d()+-]*$/;
+
 
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
     required: true,
-    minlength: 3, 
+    minlength: 3,
     maxlength: 30
   },
   lastName: {
     type: String,
     required: true,
-    minlength: 3, 
+    minlength: 3,
     maxlength: 30
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    minlength: 10, 
+    minlength: 10,
     maxlength: 30
   },
-  phoneNumber:{
-    type : Number,
-    required : true,
-    minlength : 10,
-    maxlength : 25
- },
+  phoneNumber: {
+    type: String,
+    required: true,
+    minlength: 10,
+    maxlength: 25
+  },
   password: {
     type: String,
     required: true,
-    minlength: 12, 
+    minlength: 12,
     maxlength: 100
   }
-  // Add any other fields needed for your application
+  // Add any other fields needed for the application
 });
 
 const User = mongoose.model('User', userSchema);
 
- /*function validateUser(user){
-  const schema = {
-    firstName: Joi.string().min(3).max(50).required(),
-    lastName: Joi.string().min(3).max(50).required(),
-    email: Joi.string().min(10).max(50).required(),
-    phoneNumber: Joi.string().min(10).max(25).required(),
-    password: Joi.string().min(12).max(25).required(),
-  }
-  return Joi.Validate(user, schema)
-}*/
+function validateUser(user) {
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).max(30).required(),
+    lastName: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email({ tlds: { allow: false } }).min(10).max(30).required(),
+    phoneNumber: Joi.string().regex(phoneRegex).required(),
+    password: Joi.string().min(12).max(100).required(),
+  });
+  return schema.validate(user);
+}
 
+module.exports = validateUser
 module.exports = User;
-//exports.validate = validateUser
-
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
+const validateUser = require('../models/user');
 const User = require('../models/user');
 
-// const { User, validate } = require('../models/user');
+const secretKey = process.env.JWT_SECRET_KEY;
+
 
 
 // Registration Route
-router.post('/register', async (req, res, next) => {
+/*router.post('/register', async (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
@@ -32,22 +37,41 @@ router.post('/register', async (req, res, next) => {
           });
         });
     });
- });
+ });*/
+
+router.post('/register', async (req, res, next) => {
+  const { error } = validateUser(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    if (user) return res.status(400).send('User already registered.');
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber,
+      email: req.body.email,
+      password: hashedPassword
+    });
+
+    const result = await newUser.save({ wtimeout: 30000 });
+    const token = jwt.sign({ userId: result._id }, secretKey, { expiresIn: '1h' });
+
+    res.status(201).json({
+      message: 'User created successfully!',
+      token: token,
+      result: result
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+});
 
 
-/*router.post('/register', async (req, res, next) => {
-  const { error } = validate(req.body)
-  if (error) res.status(400).send(error.details[0].message)
-      const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        phoneNumber: req.body.phoneNumber,
-        email: req.body.email,
-        password: req.body.password
-      });
-      await user.save()
-      res.send(user);
-    });*/
 
 // Login Route
 router.post('/login', (req, res, next) => {

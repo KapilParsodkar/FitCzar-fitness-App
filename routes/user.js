664c330ac95023c2956bc7ba5164945014628962
Your Name <you@example.com>
2023-02-22 23:24:00 -0500

    customizing imports

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const checkAuth = require('../middleware/check-auth');
-const User = require('../models/user'); 
+const { User } = require('../models/user'); 
 
 const router = express.Router();
 

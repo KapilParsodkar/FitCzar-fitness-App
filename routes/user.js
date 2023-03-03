51673cc627e51c37d31468fc5450342a786812e0
ArunRao1997 <rao.nayinenii@gmail.com>
2023-03-03 14:14:49 -0500

    Adding endpoints for workout

--- routes/user.js
+++ routes/user.js
@@ -5,20 +5,20 @@ const { User } = require('../models/user');
 const router = express.Router();
 
 
-router.get('/:id', checkAuth, (req, res, next) => {
-  const userId = req.params.id;
+  router.get('/:id', checkAuth, (req, res, next) => {
+    const userId = req.params.id;
 
-  User.findById(userId)
-    .then(user => {
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.status(200).json(user);
-    })
-    .catch(error => {
-      res.status(500).json({ message: 'Fetching user failed', error: error });
-    });
-});
+    User.findById(userId)
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+      })
+      .catch(error => {
+        res.status(500).json({ message: 'Fetching user failed', error: error });
+      });
+  });
 
 module.exports = router;
 

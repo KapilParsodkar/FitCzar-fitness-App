a9fb71f75da7146b6ba9a8c560a4f23a04202403
ArunRao1997 <rao.nayinenii@gmail.com>
2023-03-20 14:45:44 -0400

    Adding swagger-ui

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const swaggerUi = require('swagger-ui-express');
+// const YAML = require('yamljs');
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const workoutRoutes = require('./routes/workouts');
+// const swaggerDocument = require('./swagger.json');
 
 const app = express();
 app.use(cors()); // enable CORS for all routes
+
+// Serve Swagger UI documentation
+// const swaggerDocument = YAML.load('./swagger.yaml');
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const setupSwagger = require('./swagger');
+setupSwagger(app);
 
 mongoose.connect('mongodb://127.0.0.1/fitczar', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
---
+++ swagger.js
@@ -0,0 +1,43 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'FitCzar Fitness API',
+      version: '1.0.0',
+      description: 'API for managing user fitness data'
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local server'
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT'
+        }
+      }
+    },
+    security: [
+      {
+        bearerAuth: []
+      }
+    ]
+  },
+  apis: ['./routes/auth.js', './swagger.json']
+//   apis: ['./routes/*.js','./swagger.json']
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+function setupSwagger(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+module.exports = setupSwagger;

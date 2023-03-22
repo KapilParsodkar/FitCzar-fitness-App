a0b748c39183997e489c7b976a9ab8d7e1e5fb6d
ArunRao1997 <rao.nayinenii@gmail.com>
2023-03-22 17:50:46 -0400

    adding list of exercises

---
+++ utils/exercises.js
@@ -0,0 +1,54 @@
+const exercises = [
+    {
+      name: 'Push-ups',
+      type: 'strength',
+      muscleGroups: ['chest', 'triceps', 'shoulders'],
+    },
+    {
+      name: 'Pull-ups',
+      type: 'strength',
+      muscleGroups: ['back', 'biceps', 'forearms'],
+    },
+    {
+      name: 'Squats',
+      type: 'strength',
+      muscleGroups: ['quadriceps', 'hamstrings', 'glutes', 'calves'],
+    },
+    {
+      name: 'Lunges',
+      type: 'strength',
+      muscleGroups: ['quadriceps', 'hamstrings', 'glutes'],
+    },
+    {
+      name: 'Plank',
+      type: 'strength',
+      muscleGroups: ['core'],
+    },
+    {
+      name: 'Running',
+      type: 'cardio',
+      muscleGroups: ['legs', 'glutes', 'cardiovascular'],
+    },
+    {
+      name: 'Jumping jacks',
+      type: 'cardio',
+      muscleGroups: ['cardiovascular', 'full-body'],
+    },
+    {
+      name: 'Cycling',
+      type: 'cardio',
+      muscleGroups: ['legs', 'glutes', 'cardiovascular'],
+    },
+    {
+      name: 'Swimming',
+      type: 'endurance',
+      muscleGroups: ['full-body', 'cardiovascular'],
+    },
+    {
+      name: 'Rowing',
+      type: 'endurance',
+      muscleGroups: ['full-body', 'cardiovascular'],
+    },
+  ];
+
+  module.exports = exercises;

f82dafd214e87e6f33d867173aa906544ffbf5a5
ArunRao1997 <rao.nayinenii@gmail.com>
2023-04-24 14:12:10 -0400

    Modifying Mongo Connection String

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection = mongoose.connect('mongodb://localhost:27017/loginsignupexercisedb', {
+const connection = mongoose.connect('mongodb+srv://raonayineni:ViYA7Xtd9oLbRm2D@fitczarappcluster.spu6vxw.mongodb.net/fitczar?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });

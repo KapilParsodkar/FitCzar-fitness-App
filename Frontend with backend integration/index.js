c2aaaf6dcb4671bbfd1fffb1c8c6ed5024d38a33
ArunRao1997 <rao.nayinenii@gmail.com>
2023-04-24 10:18:32 -0400

    Encrypting the password using bcrypt

--- Frontend with backend integration/index.js
+++ Frontend with backend integration/index.js
@@ -1,72 +1,106 @@
-const express=require('express')
-const cors=require("cors");
+const express = require('express')
+const cors = require("cors");
+const bcrypt = require('bcrypt');
 require('./db/config')
-const User=require("./db/user") 
-const Exercisedb=require("./db/exercisedb")
-const app=express()
+const User = require("./db/user")
+const Exercisedb = require("./db/exercisedb")
+const app = express()
 app.use(express.json())
 app.use(cors())
-app.post("/signup",async(req,res)=>{
-   
-   let user=new User(req.body)
-   let result=await user.save()
-   result=result.toObject()
-   delete result.pwd
+
+
+app.post("/signup", async (req, res) => {
+    try {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(req.body.pwd, saltRounds);
+
+        let user = new User({
+            ...req.body,
+            pwd: hashedPassword,
+        });
+
+        let result = await user.save();
+        result = result.toObject();
+        delete result.pwd;
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ result: "internal server error" });
+    }
+});
+
+
+app.post("/login", async (req, res) => {
+    try {
+        if (req.body.pwd && req.body.email) {
+            let user = await User.findOne({ email: req.body.email }).select("+pwd");
+
+            if (user) {
+                if (req.body.pwd && user.pwd) {
+                    const passwordMatch = await bcrypt.compare(req.body.pwd, user.pwd);
+
+                    if (passwordMatch) {
+                        res.send(user);
+                    } else {
+                        res.send({ result: "incorrect password" });
+                    }
+                } else {
+                    res.send({ result: "password not found" });
+                }
+            } else {
+                res.send({ result: "no user found" });
+            }
+        } else {
+            res.send({ result: "no user found" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ result: "internal server error" });
+    }
+});
+
+
+app.post("/add-exercise", async (req, res) => {
+    let exercise = new Exercisedb(req.body)
+    let result = await exercise.save()
     res.send(result)
 })
 
-app.post("/login",async(req,res)=>{
-   if (req.body.pwd && req.body.email){
-    let user= await User.findOne(req.body).select("-password")
-    if(user){
-        res.send(user)
-    }else{res.send({result:"no user found"})}
-   }else{
-    res.send({result:"no user found"})
-   }
+
+app.get("/exercises", async (req, res) => {
+    let exercises = await Exercisedb.find()
+    if (exercises.length > 0) {
+        res.send(exercises)
+    } else {
+        res.send({ result: "no exercises found" })
+    }
 })
 
-app.post("/add-exercise",async (req,res)=>{
-  let exercise=new Exercisedb(req.body)
-  let result=await exercise.save()
-  res.send(result)
-})
+app.delete("/exercise/:id", async (req, res) => {
 
-
-app.get("/exercises",async (req,res)=>{
-   let exercises=await Exercisedb.find()
-   if(exercises.length>0){
-    res.send(exercises)
-   }else{
-    res.send({result:"no exercises found"})
-   }
-})
-
-app.delete("/exercise/:id",async(req,res)=>{
-
-    const result= await Exercisedb.deleteOne({_id:req.params.id})
+    const result = await Exercisedb.deleteOne({ _id: req.params.id })
     res.send(result)
 })
 
-app.get("/exercise/:id",async(req,res)=>{
-     let result=await Exercisedb.findOne({_id:req.params.id})
-     if(result){
+app.get("/exercise/:id", async (req, res) => {
+    let result = await Exercisedb.findOne({ _id: req.params.id })
+    if (result) {
         res.send(result)
-     }else{
-        res.send({result:"no record found"})
-     }
+    } else {
+        res.send({ result: "no record found" })
+    }
 })
 
-app.put("/exercise/:id",async(req,res)=>{
-    let result=await Exercisedb.updateOne({
-        _id:req.params.id
-    },{
-        $set:req.body
+app.put("/exercise/:id", async (req, res) => {
+    let result = await Exercisedb.updateOne({
+        _id: req.params.id
+    }, {
+        $set: req.body
     })
     res.send(result)
 })
 
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log("http://localhost:3001/")
 })
 

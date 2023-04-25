f0d30d88842ede70b4c8a8a85fcc50f32b2e2537
ArunRao1997 <rao.nayinenii@gmail.com>
2023-04-24 22:55:57 -0400

    Changing MongoDB connection to Atlas

--- Frontend with backend integration/db/config.js
+++ Frontend with backend integration/db/config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
-const connection = mongoose.connect('mongodb://localhost:27017/loginsignupexercisedb', {
+const connection = mongoose.connect('mongodb+srv://raonayineni:ViYA7Xtd9oLbRm2D@fitczarappcluster.spu6vxw.mongodb.net/fitczar?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase the timeout to 30 seconds
 });
 
 module.exports = connection;

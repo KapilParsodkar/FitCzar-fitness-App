61642f09e1503f9787016e85f597767d8e3d4ab6
ArunRao1997 <rao.nayinenii@gmail.com>
2023-04-24 20:01:02 -0400

    Ignoring node_modules

--- setupTests.js
+++ setupTests.js
@@ -4,7 +4,9 @@ beforeAll(async () => {
   const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 30000,
+    serverSelectionTimeoutMS: 30000, // Increase the timeout to 30 seconds
+    bufferCommands: false, // Disable Mongoose buffering
+    bufferMaxEntries: 0, // Set the maximum buffer entries to 0
   };
   await mongoose.connect('mongodb+srv://raonayineni:ViYA7Xtd9oLbRm2D@fitczarappcluster.spu6vxw.mongodb.net/fitczar?retryWrites=true&w=majority', options);
 });
